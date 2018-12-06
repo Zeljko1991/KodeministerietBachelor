c7851ef04a88f77a896c4a93e2a215ea4b2f1dcb
Zeljko <duranovic46@gmail.com>
2018-12-06 13:29:24 +0100

    First commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index() {
+        $title = 'Welcome to Kodeministeriet';
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function billing() {
+        $title = 'Billing';
+        return view('billing.billing')->with('title', $title);
+    }
+
+    public function marketing() {
+        $title = 'Marketing';
+        return view('marketing.marketing')->with('title', $title);
+    }
+
+    public function planning(){
+        $title = 'Planning';
+        return view('planning.planning')->with('title', $title);
+    }
+}
---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+            <a href="/" class="brand-logo">{{config('app.name', 'Kodeministeriet')}}</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/planning">Planning</a></li>
+                <li><a href="/billing">Billing</a></li>
+                <li><a href="/marketing">Marketing</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
