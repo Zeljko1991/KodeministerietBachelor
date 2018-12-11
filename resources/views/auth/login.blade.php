30b6ccbe366b368b78826940af1ce5e719bdd426
vagrant <vagrant@homestead>
2018-12-11 07:34:08 +0000

    Fixed some CSS on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                             <label for="email">{{ __('E-Mail Address') }}</label>
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    <span role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
@@ -22,7 +22,7 @@
                             <label for="password">{{ __('Password') }}</label>
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
                             @if ($errors->has('password'))
-                                <span class="invalid-feedback" role="alert">
+                                <span role="alert">
                                     <strong>{{ $errors->first('password') }}</strong>
                                 </span>
                             @endif
